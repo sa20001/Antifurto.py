51d52756d7df848fba57a3f881a87ce55a39a445
Samuele <samuele.vulcan@gmail.com>
2020-06-17 23:37:25 +0200

    Aggiunta commenti, debug, identificati errori gravi

--- Antifurto_centralina.py
+++ Antifurto_centralina.py
@@ -1,32 +1,44 @@
-#Open cfg_file e create a thread
+# Open cfg_file e create a thread
 import ast
 from threading import Thread
 import time
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-read_file = open("cfg.json", "r")
-file_read = read_file.read()
+read_file = open("cfg.json", "r")  # apre il file cfg.json in modalità read (lettura)
+file_read = read_file.read()  # creo variabile file_read che corrisponde alla lettura del file
 # print(read_file)
 # print(type(read))
 
-cfg_dict = ast.literal_eval(file_read)
+
+cfg_dict = ast.literal_eval(file_read)  # converto il la variabile file_read (str) in dizionario
 # print(type(cfg_dict))
 # print(cfg_dict)
 
-wait_time = cfg_dict["Delay_time"]
+wait_time = cfg_dict["Delay_time"]  # estraggo dal dizionario il valore della chiave delay time
 # print(wait_time)
 
-topic_base = 'afp/mr/home/'
-device_id = "Centralina_antifurto"
-broker_server = 'broker.hivemq.com'
+topic_base = 'afp/mr/home/'  # | configurazione per connettermi al broker
+device_id = "Centralina_antifurto"  # | rispetto ad antifurto_main cambio il topic in Centralina_antifurto
+broker_server = 'broker.hivemq.com'  # |
 
-i = 0
+i = 0  # inizializzo la variabile i a 0
+
+while_trigger = True  # trigger per rendere il ciclo while vero, con stato false serve per sbloccare il thread
+
+
 def code_My_Thread(nome, attesa):
-    while True:
-        global i
-        i += 1
-        print(nome + "Number of time program published to Broker: " + str(i))
+    while while_trigger:
+        global i  # la definisco global
+        i += 1  # aumenta di uno ad ogni ciclo
+        print(nome + "Number of time program published to Broker: " + str(
+            i))  # per controllo, per verificare quante volte ha pubblicato a Broker
+
+        time.sleep(
+            0.1)  # questa pausa impedisce (riduce sensibilmente) che i callback_connessione_disconnessione di questo file e Antifurto_main avvengano assieme mandando
+
+        # in crash ast e dovendo riavviare l'applicazione per farla funzionare
+        # TODO: trovare soluzione più intelligente al sleep, verificare se funziona togliendo i connect e disconnet  qui sotto, oppure semplicemente cambiano nome alle funzioni (dovrebbe funzionare la seconda strada)
 
         def callback_connessione(client, userdata, flags, rc):
             print("Connected to the server: \"", broker_server + " \" with result code", str(rc))
@@ -42,20 +54,24 @@ def code_My_Thread(nome, attesa):
         client.on_disconnect = callback_disconnessione
         client.connect(broker_server)  # si connette al broker server
 
-        publish.single(topic_base + device_id, payload="Message correctly sent: " + str(cfg_dict), hostname=broker_server)  # invio al server quello che ho ricevuto dal server
+        publish.single(topic_base + device_id, payload="Message correctly sent: " + str(cfg_dict),
+                       hostname=broker_server)  # invio al server il file cfg.json
 
         time.sleep(attesa)
 
-def test_1():
-    # definisco i thread
-    publish_centralina = Thread(target=code_My_Thread, args=("Publish to Broker--> ", wait_time)) # args qui vuole una tupla
+
+# definisco il thread
+publish_centralina = Thread(target=code_My_Thread,
+                            args=("Publish to Broker--> ", wait_time))  # args qui vuole una tupla
 
 
-    #eseguo i thread
+def Start_thread_centralina():
+    # eseguo il thread
     publish_centralina.start()
 
-    # attendo fine esecuzione dei threads
-    # publish_centralina.join() # con join() aspetta la fine di start() prima di passare all'istruzione successiva
 
-    # print("Fine esecuzione dei thread")
-
+def Stop_thread_centralina():
+    global while_trigger
+    while_trigger = False  # in questo modo rendo il ciclo while false e sblocco il thread
+    publish_centralina.join()  # killa il thread
+    print("Fine esecuzione del thread \"" + device_id + "\"")  # Messaggio di verifica
--- Antifurto_main.py
+++ Antifurto_main.py
@@ -1,29 +1,27 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
-import ast # da bytecode a dictionary
+import ast  # da bytecode a dictionary
 import Antifurto_centralina
 
 # TODO: gestione eccezioni
-# TODO: boolean file Json
 # TODO: fare in modo che se almeno un sensore è 1, il publish.single in Antifurto_centralina.py cambia lo stato della sirena a 1
 # TODO: sirena temporizzata, quindi ON tot sec, OFF tot sec
 # TODO: gestire due connesioni, fix --> non avere due utenti con stesso id
 # TODO: aggiungere supporto log, con data e ora --> meglio farlo in Android
 
 topic_base = 'afp/mr/home/'
-device_id = "SV"
+device_id = "Antifurto_Uf_SEhh"
 broker_server = 'broker.hivemq.com'
 
-server_response_dictionary = "" # variabile per salvare il json ricevuto dal server
+Antifurto_centralina.Start_thread_centralina()  # avvio il thread della centralina
 
 # faccio girare il programma in loop, fino a quando non arriva il comando "kill"
 while True:
-    server_response = 0 # variabile per estrarre la risposta dal server,
 
-    Antifurto_centralina.test_1()
+    server_response = 0  # variabile per estrarre la risposta dal server
 
     def callback_connessione(client, userdata, flags, rc):
-        print("Connected to the server: \"", broker_server + " \" with result code", str(rc))
+        print("Super Connected to the server: \"", broker_server + " \" with result code", str(rc))
         # sottoscrivo gli argomenti di interesse
         client.subscribe(topic_base + "#")
 
@@ -48,7 +46,9 @@ while True:
     while True:
         if server_response != 0: # se ho ricevuto un messaggio la variabile è diversa da 0
             dict_str = server_response.decode("UTF-8") # decodifco il byte code ricevuto secondo lo standard UTF-8
+
             server_response_dictionary = ast.literal_eval(dict_str) # converto il bytecode in dictionary
+
             print(repr(server_response_dictionary)) # The repr() function returns a printable representation of the given object. https://www.programiz.com/python-programming/methods/built-in/repr
             print(type(server_response_dictionary)) # stampo il tipo della variabile server_responde_disctionary
             break # esco dal ciclo
@@ -59,11 +59,14 @@ while True:
 
     if program_killer == "kill":
         publish.single(topic_base + device_id,
-                       payload="Message " + program_killer + " correctly received from server. System correctly terminated",
+                       payload="Message \"" + program_killer + "\" correctly received from server. System correctly terminated",
                        hostname=broker_server) # stampo il messaggio di kill
+        Antifurto_centralina.Stop_thread_centralina()  # stoppo il thread della centralina
         break # esco dal ciclo while principale
 
     publish.single(topic_base + device_id, payload="Message " + str(server_response_dictionary) + " correctly received",
                    hostname=broker_server) # invio al server quello che ho ricevuto dal server
 
-print("Fuori dal ciclo\nProgramma terminato")
+
+print("Fuori dal ciclo while main\nProgramma terminato grazie per averci testato")
+
