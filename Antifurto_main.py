056a08fe948efdf4b58979d6b14980af1b498a57
Samuele <samuele.vulcan@gmail.com>
2020-06-26 01:59:39 +0200

    Codice versione finale

--- Antifurto_main.py
+++ Antifurto_main.py
@@ -4,17 +4,87 @@ import ast  # da bytecode a dictionary
 import Antifurto_centralina
 from termcolor import colored
 
+read_file_parameters = open("Json parametrs.json", "r")  # apre il file cfg.json in modalità read (lettura)
+file_read_parameters = read_file_parameters.read()  # creo variabile file_read che corrisponde alla lettura del file
+read_file_parameters.close()
+
+server_response_dictionary = ast.literal_eval(file_read_parameters)  #inizializzo il server_response_dictionary
 server_response = None
-server_response_dictionary = 0  # inizializzata a 0 in caso di eccezione
 program_killer = None
 
+def pass_dict():
+    pass_dict_var = str(server_response_dictionary)
+    return pass_dict_var
+
+# nego i bool per riutilizzare le key dell'intent per passare le key da debug.class a sensors.class
+def pass_sensor_win0():
+    sensor_list = server_response_dictionary["sensor_list"]
+
+    if sensor_list[0]["sensor_status"] == 0:
+        # print("IL SENSORE WIN1 HA VALORE 0")
+        bool_check = not False
+    else:
+        # print("IL SENSORE WIN1 HA VALORE 1")
+        bool_check = not True
+
+    # print("STAMPO IL VALORE DI BOOL CHECK" + str(bool_check))
+    return bool_check
+
+def pass_sensor_win1():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[1]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+
+def pass_sensor_win2():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[2]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+
+def pass_sensor_win3():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[3]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+
+def pass_sensor_door():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[4]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+
+def pass_sensor_motion():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[5]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+
+def pass_siren():
+    sensor_list = server_response_dictionary["sensor_list"]
+    if sensor_list[6]["sensor_status"] == 0:
+        bool_check = not False
+    else:
+        bool_check = not True
+    return bool_check
+# nego i bool per riutilizzare le key dell'intent per passare le key da debug.class a sensors.class
 
 def main_program():
+
     global server_response
     global server_response_dictionary
     global program_killer
 
-    # TODO: aggiungere supporto log, con data e ora --> meglio farlo in Android
     Antifurto_centralina.Start_thread_centralina()  # avvio il thread della centralina
 
     # faccio girare il programma in loop, fino a quando non arriva il comando "kill"
@@ -23,21 +93,24 @@ def main_program():
         server_response = 0  # variabile per estrarre la risposta dal server
         program_killer = 0  # inizializzata a 0 in caso di eccezione
 
+
         def callback_connessione(client, userdata, flags, rc):
-            print("Super Connected to the server: \"", Antifurto_centralina.broker_server + " \" with result code",
-                  str(rc))
+            print("Super Connected to the server: \"" +  Antifurto_centralina.broker_server + " \" with result code " + str(rc))
             # sottoscrivo gli argomenti di interesse
             client.subscribe(Antifurto_centralina.topic_base + "#")
+
 
         def callback_disconnessione(client, userdata, rc):
             if rc != 0:
                 print('Disconnesso')
+
 
         def callback_ricezione(client, userdata, message):
             print(userdata)
             print("Message received from " + message.topic + " with content " + str(message.payload))
             global server_response  # variabile definita global per estrarre la risposta del server dalla funzione
             server_response = message.payload  # estraggo la risposta da message.payload e la salvo in questa variabile
+
 
         client = mqtt.Client(client_id="AFP")
         client.on_connect = callback_connessione  # Define the connect callback implementation.
@@ -54,8 +127,7 @@ def main_program():
             try:  # gestione eccezione per il metodo literal_eval (a volte va in crash) e per bad json format
 
                 if server_response != 0:  # se ho ricevuto un messaggio la variabile è diversa da 0
-                    dict_str = server_response.decode(
-                        "UTF-8")  # decodifco il byte code ricevuto secondo lo standard UTF-8
+                    dict_str = server_response.decode("UTF-8")  # decodifco il byte code ricevuto secondo lo standard UTF-8
                     server_response_dictionary = ast.literal_eval(dict_str)  # converto il bytecode in dictionary
 
                     # The repr() function returns a printable representation of the given object.
@@ -65,8 +137,6 @@ def main_program():
 
                     checker = False
 
-                    # TODO: da qui creare array di sensori in android, estrarre dati da "server_response_dictionary"
-                    #  e dal file cfg.json
                     for i in server_response_dictionary["sensor_list"]:  # stampo tutti valori della lista dei sensori
 
                         if i["sensor_status"] == 0:  # se il sensore non ha rilevato nulla
@@ -81,13 +151,11 @@ def main_program():
                                            hostname=Antifurto_centralina.broker_server)
 
                     if checker:
-                        for index, element in enumerate(
-                                server_response_dictionary["sensor_list"]):  # entro nella lista del dict
+                        for index, element in enumerate(server_response_dictionary["sensor_list"]):  # entro nella lista del dict
                             if element["sensor_id"] == "Siren":
                                 server_response_dictionary["sensor_list"][index][
                                     "sensor_status"] = 1  # cambia lo stato a 1 all'interno del dict
-                                print(colored(server_response_dictionary, "magenta"))  # debug
-                    # TODO: fino a qui creare array di sensori in android
+                                print(colored(server_response_dictionary, "magenta"))   # debug
 
                     break  # esco dal ciclo
 
@@ -102,8 +170,7 @@ def main_program():
 
             try:  # gestisco l'eccezione se non è presente la chiave "System_status" nel file JSON
                 program_killer = server_response_dictionary[
-                    "System_status"]  # estraggo il valore della chiave "System_status" e lo associo alla variabile
-                # programm_killer
+                    "System_status"]  # estraggo il valore della chiave "System_status" e lo associo alla variabile programm_killer
 
             except KeyError:  # gestisco l'eccezione solo se è KeyError
                 print(colored("Wrong key \"System_status\", maybe mistyped or missing\nCheck JSON sent", "red"))
@@ -113,16 +180,14 @@ def main_program():
                 print(colored("Command \"kill\" received from Broker", "red"))
 
                 publish.single(Antifurto_centralina.topic_base + Antifurto_centralina.device_id_kill,
-                               payload="Message \"" + program_killer + "\" correctly received from server. System "
-                                                                       "correctly terminated",
+                               payload="Message \"" + program_killer + "\" correctly received from server. System correctly terminated",
                                hostname=Antifurto_centralina.broker_server)  # stampo il messaggio di kill
                 Antifurto_centralina.Stop_thread_centralina()  # stoppo il thread della centralina
                 break  # esco dal ciclo while principale
 
             # invio al server quello che ho ricevuto dal server in un solo messaggio
-            publish.single(Antifurto_centralina.topic_base + Antifurto_centralina.device_id_check,
-                           payload="Message " + str(server_response_dictionary) +
-                                   " correctly received", hostname=Antifurto_centralina.broker_server)
+            publish.single(Antifurto_centralina.topic_base + Antifurto_centralina.device_id_check, payload="Message " + str(server_response_dictionary) +
+                                                                                                           " correctly received", hostname=Antifurto_centralina.broker_server)
 
         else:
             # if errors occurred during conversion from bytecode to dicitonary
@@ -132,13 +197,10 @@ def main_program():
             # Publish multiple messages to a broker, then disconnect cleanly.
             multiple_publish = [{'topic': Antifurto_centralina.topic_base + Antifurto_centralina.device_id_error,
                                  'payload': "An error occurred"},
-                                (Antifurto_centralina.topic_base + Antifurto_centralina.device_id_error_message,
-                                 "With message BAD JSON FORMAT", 0, False)]
-            publish.multiple(multiple_publish,
-                             hostname=Antifurto_centralina.broker_server)  # invio al server più messaggi nello
-            # stesso momento
+                                (Antifurto_centralina.topic_base + Antifurto_centralina.device_id_error_message, "With message BAD JSON FORMAT", 0, False)]
+            publish.multiple(multiple_publish, hostname=Antifurto_centralina.broker_server)  # invio al server più messaggi nello stesso momento
 
     print("Fuori dal ciclo while main\nProgramma terminato grazie per averci testato")
 
-
-main_program()
+def on_Destroy():
+    Antifurto_centralina.Stop_thread_centralina()
--- all_sensors_status.py
+++ all_sensors_status.py
@@ -1,1 +1,12 @@
-# TODO: aggiungere qui tutti gli stati finali che poi andranno utilizzati da android
+# TODO: creare l'interfaccia grafica basandosi su questo file
+import Antifurto_main
+
+Antifurto_main.main_program()   # per iniziare tutto
+# Antifurto_main.on_Destroy()  # per killare tutto
+# Antifurto_main.pass_sensor_win0()   # per passare gli stati dei sensori
+# Antifurto_main.pass_sensor_win1()   # per passare gli stati dei sensori
+# Antifurto_main.pass_sensor_win2()   # per passare gli stati dei sensori
+# Antifurto_main.pass_sensor_win3()   # per passare gli stati dei sensori
+# Antifurto_main.pass_sensor_door()   # per passare gli stati dei sensori
+# Antifurto_main.pass_sensor_motion() # per passare gli stati dei sensori
+# Antifurto_main.pass_siren() # per passare lo stato della sirena
